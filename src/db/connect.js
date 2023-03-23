792796cc2a6dd8233b613caadb16dfd70d84ee37
ellis <lsp1@hotmail.com>
2023-03-23 10:27:26 -0600

    uri for mongo atlas connect

--- src/db/connect.js
+++ src/db/connect.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connectToDb = async () => {
   try {
-    await mongoose.connect(process.env.URL, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
