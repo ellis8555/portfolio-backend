69b4286d06e0176699bc9c4cbc52549cddddcf1e
ellis <lsp1@hotmail.com>
2023-03-23 10:45:32 -0600

    server.js connectToDB.then added

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 require("dotenv").config();
-const mongoose = require("mongoose");
 const express = require("express");
 const { connectToDb } = require("./db/connect");
 
@@ -7,17 +6,16 @@ const { connectToDb } = require("./db/connect");
 const app = express();
 const PORT = process.env.PORT || 3500;
 
+// get connection to the database
+connectToDb().then(() => {
+  console.log("Connected to MongoDB");
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
+
 // middleware
 app.use(express.json());
-
-// get connection to the database
-connectToDb();
 
 // routes
 app.use("/contact", require("./routes/contact"));
 
 // server listening
-mongoose.connection.once("open", () => {
-  console.log("Connected to MongoDB");
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-});
