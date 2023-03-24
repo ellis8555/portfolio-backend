3e860a827d4dfd4b2b44b2a4859141de83d8a33c
ellis <lsp1@hotmail.com>
2023-03-24 12:18:51 -0600

    contact schema validations added

--- src/schemas/contact-me.js
+++ src/schemas/contact-me.js
@@ -5,12 +5,12 @@ const contactMeMessage = new mongoose.Schema(
     name: {
       type: String,
       minLength: [2, "minimum of 2 characters required"],
-      maxLength: [20, "maximum of 20 characters allowed"],
+      maxLength: [20, "maximum of 25 characters allowed"],
       required: true,
     },
     userMessage: {
       type: String,
-      maxLength: [200, `max character limit is 200`],
+      maxLength: [250, `max character limit is 250`],
       required: true,
     },
   },
