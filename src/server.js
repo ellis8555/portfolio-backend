f8632081abe55fbedfad09af6170ec4aa16107f2
ellis <lsp1@hotmail.com>
2023-03-24 12:28:28 -0600

    server.js matches live server

--- src/server.js
+++ src/server.js
@@ -1,31 +1,19 @@
 require("dotenv").config();
-const mongoose = require("mongoose");
 const express = require("express");
-const cors = require("cors");
 const { connectToDb } = require("./db/connect");
 
 // variables
 const app = express();
 const PORT = process.env.PORT || 3500;
 
-// middleware
-// cors
-app.use(
-  cors({
-    origin: "http://localhost:9000",
-  })
-);
-// json
-app.use(express.json());
-
 // get connection to the database
-connectToDb();
-
-// routes
-app.use("/contact", require("./routes/contact"));
-
-// server listening
-mongoose.connection.once("open", () => {
+connectToDb().then(() => {
   console.log("Connected to MongoDB");
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
+
+// middleware
+app.use(express.json());
+
+// routes
+app.use("/contact", require("./routes/contact"));
