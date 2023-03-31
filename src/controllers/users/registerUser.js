2f65a7ed75e30fe5ac74fe07e04a7ba5810be0c4
ellis <lsp1@hotmail.com>
2023-03-31 08:12:50 -0600

    create registerUser.js

---
+++ src/controllers/users/registerUser.js
@@ -0,0 +1,43 @@
+const bcrypt = require("bcrypt");
+const User = require("../../schemas/users");
+
+const registerUser = async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username) {
+    res
+      .status(400)
+      .json({ message: "Please enter a username", field: "username" });
+  }
+  if (!password) {
+    res
+      .status(400)
+      .json({ message: "Please enter password", field: "password" });
+  }
+
+  try {
+    const doesUserNameExist = await User.findOne({ username });
+    if (!doesUserNameExist) {
+      const hashedPassword = await bcrypt.hash(password, 13);
+      const credentials = {
+        username,
+        password: hashedPassword,
+      };
+      const newUser = new User(credentials);
+      await newUser.save();
+      res.status(201).json({
+        username,
+        message: `${username} your account has been created!`,
+      });
+    } else {
+      res.status(409).json({
+        message: "That username is already taken. Please try another username",
+        field: "duplicate",
+      });
+    }
+  } catch (e) {
+    res.status(400).json({ error: e.errors });
+  }
+};
+
+module.exports = registerUser;
