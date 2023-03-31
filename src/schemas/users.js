ee6d0cefc46c27a1f7d6d62b683c4f8d7f65bd06
ellis <lsp1@hotmail.com>
2023-03-31 08:12:01 -0600

    userschema/routes name changed i think

---
+++ src/schemas/users.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const User = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      minLength: [2, "minimum of 2 characters required"],
+      maxLength: [30, "maximum of 30 characters allowed"],
+      required: true,
+    },
+    password: {
+      type: String,
+      minLength: [8, "A minimum password length of 8 is required"],
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("user", User);
